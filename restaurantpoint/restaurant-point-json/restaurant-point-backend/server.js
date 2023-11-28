8a4cc97a05a2410fc15f033a7045fcfa9b77707f
ylberuksmajli <ukzmaili01@gmail.com>
2023-11-28 18:37:23 +0100

    Restaurant point 0.1v
    Restaurant point 0.1v

---
+++ restaurantpoint/restaurant-point-json/restaurant-point-backend/server.js
@@ -0,0 +1,22 @@
+
+const jsonServer = require("json-server");
+const jsonServerPort = 8077;
+
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+
+ // I kam komentu per shkak se me u ba run Serveri .
+/*
+const menuItemsRoutes = require("./routes/menu-items");
+const menusRoutes = require("./routes/menus");
+const restaurantsRoutes = require("./routes/restaurants");
+
+menuItemsRoutes(server);
+menusRoutes(server);
+restaurantsRoutes(server); */
+
+server.listen(jsonServerPort,() => {
+  console.log(`JSon server running on port ${jsonServerPort}`);
+});
